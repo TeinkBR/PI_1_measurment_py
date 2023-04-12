efc555cb000f62af93212558e5dcfbc23952b465
Jingyi Li <33937631+TeinkBR@users.noreply.github.com>
2023-04-12 19:49:27 +0200

    some updates

---
+++ Keithley2182 nanovoltmeter.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This code is used for controlling the KEITHLEY 2182A Nanovoltmeter
+
+# In[1]:
+
+
+get_ipython().system('pip install -U pyvisa')
+
+
+# In[ ]:
+
+
+get_ipython().system('pip uninstall -y pyvisa')
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install pyvisa')
+
+
+# In[2]:
+
+
+import visa
+
+class KeithleyController:
+    def __init__(self, gpib_address):
+        rm = visa.ResourceManager()
+        self.keithley = rm.open_resource(gpib_address)
+        
+    def reset(self):
+        self.keithley.write("*rst; status:preset; *cls")
+        
+    def set_measurement_interval(self, interval_in_ms):
+        self.keithley.write(f"trigger:delay {interval_in_ms / 1000}")
+        
+    def set_number_of_readings(self, number_of_readings):
+        self.keithley.write(f"sample:count {number_of_readings}")
+        self.keithley.write("trace:points %d" % number_of_readings)
+        
+    def enable_measurement(self):
+        self.keithley.write("status:measurement:enable 512; *sre 1")
+        
+    def set_trigger_source(self, source):
+        self.keithley.write(f"trigger:source {source}")
+        
+    def set_trace_feed(self, feed):
+        self.keithley.write(f"trace:feed {feed}; feed:control next")
+        
+    def initiate_measurement(self):
+        self.keithley.write("initiate")
+        
+    def assert_trigger(self):
+        self.keithley.assert_trigger()
+        
+    def wait_for_srq(self):
+        self.keithley.wait_for_srq()
+
+
+# In[ ]:
+
+
+keithley = KeithleyController("GPIB::12")
+keithley.reset()
+keithley.set_measurement_interval(500)
+keithley.set_number_of_readings(10)
+keithley.enable_measurement()
+keithley.set_trigger_source("bus")
+keithley.set_trace_feed("sense1")
+keithley.initiate_measurement()
+keithley.assert_trigger()
+keithley.wait_for_srq()
+
---
+++ Keithley_2612_B_System_source_meter.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install -U pyvisa')
+
+
+# In[4]:
+
+
+pip install pyvisa
+
+
+# In[6]:
+
+
+import pyvisa as visa
+
+class Keithley2612BController:
+    def __init__(self, ip_address):
+        self.keithley = visa.ResourceManager().open_resource(f"TCPIP0::{ip_address}::5025::SOCKET")
+        
+    def reset(self):
+        self.keithley.write("smua.reset()")
+        
+    def set_source_function(self, function):
+        self.keithley.write(f"smua.source.func=smua.{function}")
+        
+    def set_source_voltage(self, voltage):
+        self.keithley.write(f"smua.source.levelv={voltage}")
+        
+    def set_source_voltage_range(self, voltage_range):
+        self.keithley.write(f"smua.source.rangev={voltage_range}")
+        
+    def set_source_current_limit(self, current_limit):
+        self.keithley.write(f"smua.source.limiti={current_limit}")
+        
+    def set_measurement_current_range(self, current_range):
+        self.keithley.write(f"smua.measure.rangei={current_range}")
+        
+    def enable_source_output(self):
+        self.keithley.write("smua.source.output=smua.OUTPUT_ON")
+        
+    def disable_source_output(self):
+        self.keithley.write("smua.source.output=smua.OUTPUT_OFF")
+        
+    def measure_current(self):
+        return self.keithley.ask("smua.measure.i()")
+
+
+# In[7]:
+
+
+k2612B = Keithley2612BController("169.254.000.001")
+k2612B.reset()
+k2612B.set_source_function("OUTPUT_DCVOLTS")
+k2612B.set_source_voltage_range(20)
+k2612B.set_source_voltage(5)
+k2612B.set_source_current_limit(10e-3)
+k2612B.set_measurement_current_range(10e-3)
+k2612B.enable_source_output()
+current = k2612B.measure_current()
+k2612B.disable_source_output()
+k2612B.reset()
+
+
+# In[ ]:
+
+
+
+
